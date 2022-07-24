414e65b6624c07d76fa384289203490fa802b7d8
inker <anton.veselyev@gmail.com>
2022-07-24 14:43:39 +0100

    no-this-brackets

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import moduleImportOrder from './rules/module-import-order'
 import noObjectAssignThis from './rules/no-object-assign-this'
 import noSameLineForElements from './rules/no-same-line-for-elements'
+import noThisBrackets from './rules/no-this-brackets'
 import singleImportPerLine from './rules/single-import-per-line'
 
 export = {
@@ -8,6 +9,7 @@ export = {
     'module-import-order': moduleImportOrder,
     'no-object-assign-this': noObjectAssignThis,
     'no-same-line-for-elements': noSameLineForElements,
+    'no-this-brackets': noThisBrackets,
     'single-import-per-line': singleImportPerLine,
   },
 }
---
+++ src/rules/no-this-brackets.ts
@@ -0,0 +1,28 @@
+import {
+  type Rule,
+} from 'eslint'
+
+export default {
+  create(context) {
+    return {
+      MemberExpression(node) {
+        const {
+          object,
+          computed,
+        } = node
+
+        const isError = object.type === 'ThisExpression'
+          && !computed
+
+        if (!isError) {
+          return
+        }
+
+        context.report({
+          node,
+          message: 'Do not use "this[computedKey]".',
+        })
+      },
+    }
+  },
+} as Rule.RuleModule

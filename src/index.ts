c2588d9e1a2d87e23a9797384a829e73a0339cff
Anton Veselev <anton.veselyev@gmail.com>
2023-04-30 03:17:58 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import moduleImportOrder from './rules/module-import-order'
 import noObjectAssignThis from './rules/no-object-assign-this'
 import noSameLineForElements from './rules/no-same-line-for-elements'
 import noThisBrackets from './rules/no-this-brackets'
-import noTrueAsDefaultValue from './rules/no-true-as-default-value'
+import noTrueAsDefaultValue from './rules/no-true-as-default'
 import singleImportPerLine from './rules/single-import-per-line'
 
 export = {
@@ -11,7 +11,7 @@ export = {
     'no-object-assign-this': noObjectAssignThis,
     'no-same-line-for-elements': noSameLineForElements,
     'no-this-brackets': noThisBrackets,
-    'no-true-as-default-value': noTrueAsDefaultValue,
+    'no-true-as-default': noTrueAsDefaultValue,
     'single-import-per-line': singleImportPerLine,
   },
 }

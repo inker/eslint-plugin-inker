2c760a4afb6c96b3a4d9418c86c8cf0f8100e0c5
Anton Veselev <anton.veselyev@gmail.com>
2023-04-30 03:10:42 +0100

    no-true-as-default-value

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import moduleImportOrder from './rules/module-import-order'
 import noObjectAssignThis from './rules/no-object-assign-this'
 import noSameLineForElements from './rules/no-same-line-for-elements'
 import noThisBrackets from './rules/no-this-brackets'
+import noTrueAsDefaultValue from './rules/no-true-as-default-value'
 import singleImportPerLine from './rules/single-import-per-line'
 
 export = {
@@ -10,6 +11,7 @@ export = {
     'no-object-assign-this': noObjectAssignThis,
     'no-same-line-for-elements': noSameLineForElements,
     'no-this-brackets': noThisBrackets,
+    'no-true-as-default-value': noTrueAsDefaultValue,
     'single-import-per-line': singleImportPerLine,
   },
 }
---
+++ src/rules/no-true-as-default-value.ts
@@ -0,0 +1,27 @@
+import type {
+  Rule,
+} from 'eslint'
+
+export default {
+  create(context) {
+    return {
+      AssignmentPattern(node) {
+        const {
+          right,
+        } = node
+
+        const isError = right.type === 'Literal'
+          && right.value === true
+
+        if (!isError) {
+          return
+        }
+
+        context.report({
+          node: right,
+          message: 'Do not use true as a default value',
+        })
+      },
+    }
+  },
+} as Rule.RuleModule
